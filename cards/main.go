2fea73a7acce62cbfda721e1b037b2deff5ebe95
didikz <rezd14@gmail.com>
2021-05-28 08:11:35 +0700

    helper for convert deck to string and cast to joined string

--- cards/main.go
+++ cards/main.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func main() {
 	cards := newDeck()
+	cardToString := cards.toString()
+
+	fmt.Println(cardToString)
+	fmt.Println(" ")
 
 	hand, remainingCards := deal(cards, 5)
 
@@ -11,6 +15,7 @@ func main() {
 	fmt.Println(" ")
 	remainingCards.print()
 
+	// slice byte
 	greeting := "hi there!"
 	fmt.Println([]byte(greeting))
 }
