9331b644d2cc324665cb4d765d0a4a34985a8728
didikz <rezd14@gmail.com>
2021-11-10 06:43:43 +0700

    enhance generator

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // create a new type of 'deck
@@ -63,8 +64,12 @@ func newDeckfromFile(filename string) deck {
 }
 
 func (d deck) shuffle() {
+	// create random generator seed source to make sure its always get different result
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 
 		//swap value
 		d[i], d[newPosition] = d[newPosition], d[i]
