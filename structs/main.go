d56fee44030383b8c5bc495cf26df5ac6eb88d03
didikz <rezd14@gmail.com>
2021-11-21 21:04:42 +0700

    updating struct declaration method

--- structs/main.go
+++ structs/main.go
@@ -13,6 +13,14 @@ func main() {
 	alex := person{"Alex", "Anderson", 30, "brooklyn"}                                // rely on properties order
 	moshi := person{firstName: "Moshi", lastName: "Meong", age: 4, address: "Malang"} // specify based on property key
 
+	// use this kind of declaration
+	var molly person
+	molly.firstName = "Molly"
+	molly.lastName = "Ndut"
+	molly.age = 5
+	molly.address = "malang"
+
 	fmt.Println(alex)
-	fmt.Print(moshi)
+	fmt.Println(moshi)
+	fmt.Printf("%+v", molly)
 }
