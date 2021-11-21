b53ad62f11591c75a6e090bdc589d5b5b4e88fd8
didikz <rezd14@gmail.com>
2021-11-21 21:10:28 +0700

    multiple struct

--- structs/main.go
+++ structs/main.go
@@ -2,16 +2,22 @@ package main
 
 import "fmt"
 
+type contact struct {
+	email string
+	phone string
+}
+
 type person struct {
 	firstName string
 	lastName  string
 	age       int
 	address   string
+	contact   contact
 }
 
 func main() {
-	alex := person{"Alex", "Anderson", 30, "brooklyn"}                                // rely on properties order
-	moshi := person{firstName: "Moshi", lastName: "Meong", age: 4, address: "Malang"} // specify based on property key
+	alex := person{"Alex", "Anderson", 30, "brooklyn", contact{"alex@gmail.com", "01234556"}}                                         // rely on properties order
+	moshi := person{firstName: "Moshi", lastName: "Meong", age: 4, address: "Malang", contact: contact{"moshi@gmail.com", "0123123"}} // specify based on property key
 
 	// use this kind of declaration
 	var molly person
@@ -19,6 +25,7 @@ func main() {
 	molly.lastName = "Ndut"
 	molly.age = 5
 	molly.address = "malang"
+	molly.contact = contact{}
 
 	fmt.Println(alex)
 	fmt.Println(moshi)
